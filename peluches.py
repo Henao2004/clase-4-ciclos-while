2deca02cf149ac0990fdd405b22f35c208dcd4b0
juan jose quiroz <juanjose1033205@gmail.com>
2023-08-25 11:23:38 -0500

    NT4 se crearon ciclos while

---
+++ peluches.py
@@ -0,0 +1,27 @@
+peluches=["johan","goku","krilin"]
+opcion=0
+print("pelucheria PELUCHITOS")
+print("........")
+print("1. Agregarproducto a la bodega")
+print("2. Ver productos de bodegas")
+print("3. Obtener valor del inventario")
+print("4. Ver productos mas vendidos")
+print("5. Salir")
+while(opcion!=5):
+    opcion=int(input("Digita un numero: "))
+    
+    
+    if(opcion==1):
+        print("usted en la opcion 1")
+    elif(opcion==2):
+        print("usted esta en la opcion 2")
+    elif(opcion==3):
+        print("usted esta en la opcion 2")
+    elif(opcion==4):
+        print("usted esta en la opcion 2")
+    elif(opcion==5):
+        print("el programa termino")
+    elif(opcion<=1,opcion>5):
+        print("la opcion que digito no existe")
+   
+    
